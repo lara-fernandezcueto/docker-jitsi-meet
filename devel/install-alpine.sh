c16db7ce3aefe3e60a649a049b2c161f5d882982
Lara Fernandez Cueto <lara.fernandezcueto@strato.de>
2024-06-05 14:42:47 +0200

    Only copy files interpreting wildcards

--- devel/install-alpine.sh
+++ devel/install-alpine.sh
@@ -16,10 +16,19 @@ for ifile in $(find ${SRCDIR}/debian -name "*install") ; do
       continue
     fi
     mkdir -p "${DESTDIR}/${subdir}/${dest}"
-    if [ -f "${SRCDIR}/${orig}" ] ; then
-      cp -a ${SRCDIR}/${orig} "${DESTDIR}/${subdir}/${dest}"
+    if [ "$(basename "${orig}")" = "*" ] ; then
+      dir=$(dirname "${orig}")
+      for f in $(find ${SRCDIR}/${dir} ${TMPDIR}/${dir} -type f) ; do
+        cp -a $f ${DESTDIR}/${subdir}/${dest}
+      done
     else
-      cp -a ${TMPDIR}/${orig} "${DESTDIR}/${subdir}/${dest}"
+      f=$(find ${SRCDIR} ${TMPDIR} -path "*${orig}")
+      if [ "${f}" ] ;then
+        cp -a $f "${DESTDIR}/${subdir}/${dest}"
+      else
+        echo "${orig} not found in ${SRCDIR} ${TMPDIR}" >&2
+        exit 1
+      fi
     fi
   done < "${ifile}"
 done
